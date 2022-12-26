3b4de3628dfce595f47f592d69cc240d276d5b7d
Tomasz Hołubowicz <mail@alternateved.com>
2022-12-26 18:10:17 +0100

    clojure: Simplify accumulate solution

--- clojure/accumulate/src/accumulate.clj
+++ clojure/accumulate/src/accumulate.clj
@@ -1,11 +1,9 @@
 (ns accumulate)
 
 (defn accumulate
-  ([f coll]
-   (if (empty? coll)
-     '()
-     (accumulate f '() coll)))
-  ([f accum [head & tail]]
-   (if (empty? tail)
-     (reverse (conj accum (f head)))
-     (recur f (conj accum (f head)) tail))))
+  [f initial]
+  (loop [coll initial
+         acc []]
+    (if (empty? coll) acc
+        (recur  (rest coll) (conj acc (f (first coll)))))))
+
