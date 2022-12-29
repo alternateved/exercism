faded4ae62c46b077ab86603654610a28c5e2008
Tomasz Hołubowicz <mail@alternateved.com>
2022-12-29 19:43:53 +0100

    clojure: Improve series solution even more

--- clojure/series/src/series.clj
+++ clojure/series/src/series.clj
@@ -1,10 +1,10 @@
-(ns series)
+(ns series
+  (:require [clojure.string :as str]))
 
 (defn slices
   "Given a string of digits, output all the contiguous substrings
   of provided length in that string in the order that they appear."
-  [string length]
-  (if (= length 0) [""]
-      (vec (map #(apply str %)
-                (partition length 1 string)))))
-
+  [s n]
+  (if (zero? n) [""]
+      (mapv str/join
+            (partition n 1 s))))
