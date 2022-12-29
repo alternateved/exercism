47f996b0df3efa9a7682f6a5437ce11a0c81cebb
Tomasz Hołubowicz <mail@alternateved.com>
2022-12-29 17:54:20 +0100

    clojure: Improve series solution

--- clojure/series/src/series.clj
+++ clojure/series/src/series.clj
@@ -4,12 +4,7 @@
   "Given a string of digits, output all the contiguous substrings
   of provided length in that string in the order that they appear."
   [string length]
-  (loop [acc []
-         s string]
-    (let [s-length (count s)]
-      (cond
-        (= length 0) [""]        
-        (= s-length 0) []
-        (< s-length length) []
-        (= s-length length) (conj acc (apply str s))
-        :else (recur (conj acc (apply str (take length s))) (rest s))))))
+  (if (= length 0) [""]
+      (vec (map #(apply str %)
+                (partition length 1 string)))))
+
