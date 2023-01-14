e9ce6c31a4f8943d5881ea7bbb8ecb9ad67a3859
Tomasz Hołubowicz <mail@alternateved.com>
2023-01-14 23:28:43 +0100

    clojure: Add docstrings to scrabble-score solution

--- clojure/scrabble-score/src/scrabble_score.clj
+++ clojure/scrabble-score/src/scrabble_score.clj
@@ -1,15 +1,17 @@
 (ns scrabble-score
   (:require [clojure.string :as str]))
 
-(defn score-letter [c]
-  (case (str (str/lower-case c))
-    ("a" "e" "i" "o" "u" "l" "n" "r" "s" "t") 1
-    ("d" "g") 2
-    ("b" "c" "m" "p") 3
-    ("f" "h" "v" "w" "y") 4
-    "k" 5
-    ("j" "x") 8
-    ("q" "z") 10))
+(defn score-letter
+  "Return score of single letter."
+  [c] (case (str (str/lower-case c))
+        ("a" "e" "i" "o" "u" "l" "n" "r" "s" "t") 1
+        ("d" "g") 2
+        ("b" "c" "m" "p") 3
+        ("f" "h" "v" "w" "y") 4
+        "k" 5
+        ("j" "x") 8
+        ("q" "z") 10))
 
-(defn score-word [s]
-  (transduce (map score-letter) + 0 s))
+(defn score-word
+  "Return score of a word."
+  [s] (transduce (map score-letter) + 0 s))
