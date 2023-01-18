5e36218707c622ebb097242a3d60969c85d1e54d
Tomasz Hołubowicz <mail@alternateved.com>
2023-01-18 22:31:53 +0100

    clojure: Add second part of run-length-encoding

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -8,10 +8,23 @@
              :let [n (count l)]]
          (conj [n] (first l)))
        (map (fn [[x & rest :as elem]]
-              (if (= 1 x) rest elem)))       
+              (if (= 1 x) rest elem)))
        (map str/join)
-       (apply str)))
+       (str/join)))
+
+(defn decode-single
+  "Decodes a single pair of number and character or character."
+  [s]
+  (if (= 1 (count s))
+    s
+    (let [n (Integer. (re-find #"\d+" s))
+          c (re-find #"[a-zA-Z ]" s)]
+      (str/join (repeat n c)))))
 
 (defn run-length-decode
   "Decodes a run-length-encoded string."
-  [cipher-text])
+  [s]
+  (->> (re-seq #"(\d+[a-zA-Z ]{1})|(?<=[a-zA-Z ]{1})[a-zA-Z ]{1}|(^[a-zA-Z ]{1})" s)
+       (map first)
+       (map decode-single)
+       (str/join)))
