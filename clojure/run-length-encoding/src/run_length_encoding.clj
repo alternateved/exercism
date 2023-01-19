a33fd33c251217909828befe6e9d82834b130dd6
Tomasz Hołubowicz <mail@alternateved.com>
2023-01-19 14:51:53 +0100

    clojure: Improve run-length-encoding solution

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -3,28 +3,17 @@
 
 (defn run-length-encode
   "Encodes a string with run-length-encoding."
-  [s]
-  (->> (for [l (partition-by identity s)
-             :let [n (count l)]]
-         (conj [n] (first l)))
-       (map (fn [[x & rest :as elem]]
-              (if (= 1 x) rest elem)))
-       (map str/join)
-       (str/join)))
+  [s] (->> (partition-by identity s)
+           (mapcat (juxt count first))
+           (remove #{1})
+           (str/join)))
 
 (defn decode-single
   "Decodes a single pair of number and character or character."
-  [s]
-  (if (= 1 (count s))
-    s
-    (let [n (Integer. (re-find #"\d+" s))
-          c (re-find #"[a-zA-Z ]" s)]
-      (str/join (repeat n c)))))
+  [[_ n c]] (if-not n c (repeat (Long/parseLong n) c)))
 
 (defn run-length-decode
   "Decodes a run-length-encoded string."
-  [s]
-  (->> (re-seq #"(\d+[a-zA-Z ]{1})|(?<=[a-zA-Z ]{1})[a-zA-Z ]{1}|(^[a-zA-Z ]{1})" s)
-       (map first)
-       (map decode-single)
-       (str/join)))
+  [s] (->> (re-seq #"(\d+)?(\w|\s)" s)
+           (mapcat decode-single)
+           (str/join)))
