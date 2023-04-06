239606b8495062acc205c73230a40e1cb23b3138
Tomasz Hołubowicz <mail@alternateved.com>
2023-04-06 21:01:15 +0200

    zig: Add grains solution

---
+++ zig/grains/grains.zig
@@ -0,0 +1,14 @@
+const math = @import("std").math;
+
+pub const ChessboardError = error{IndexOutOfBounds};
+
+pub fn square(index: usize) ChessboardError!u64 {
+    if (index < 1 or index > 64)
+        return ChessboardError.IndexOutOfBounds;
+
+    return math.pow(u64, 2, index - 1);
+}
+
+pub fn total() u64 {
+    return math.maxInt(u64);
+}
