554373e045086b102125094cd185af89ecd04125
Tomasz Hołubowicz <mail@alternateved.com>
2023-11-02 22:39:56 +0100

    ruby: Add hamming solution

---
+++ ruby/hamming/hamming.rb
@@ -0,0 +1,10 @@
+module Hamming
+  def self.compute(strand_a, strand_b)
+    raise ArgumentError.new if strand_a.length != strand_b.length
+
+    strand_a
+      .chars
+      .zip(strand_b.chars)
+      .count { |a, b| a != b }
+  end
+end
